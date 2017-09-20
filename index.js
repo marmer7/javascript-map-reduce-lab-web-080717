51f485d5033c086f995a86047a760c8d08082b47
Marlon Merjos <marlonmerjos@gmail.com>
2017-09-20 16:11:09 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,36 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+var issuesWithUpdatedApiUrl = issues.map(function callback(issue, index, array) {
+  // obj = issue
+  // obj.url.replace('api.github.com', 'api-v2.github.com')
+  // return obj
+  return Object.assign({}, issue, {url: issue.url.replace('api.github.com', 'api-v2.github.com')})
+});
+
+var commentCountAcrossIssues = issues.map(issue => issue.comments_count).reduce(function counter(sum, num) {return sum + num}, 0);
+
+var openIssues = issues.reduce(function callback(openIssues, issue){
+  if (issue.state === 'open') {
+    return [...openIssues, issue];
+  }
+  return [...openIssues]
+}, [])
+
+var nonAutomaticIssues = issues.reduce(function callback(nonAutomaticIssues, issue){
+  if (issue.body !== "This pull request has been automatically created by learn.co.") {
+    return [...nonAutomaticIssues, issue];
+  }
+  return [...nonAutomaticIssues]
+}, [])
+
+const $tbody = document.getElementById('results');
+$tbody.innerHTML = nonAutomaticIssues
+  .map(issue => `<tr>
+    <td>${issue.body}</td>
+    <td>${issue.created_at}</td>
+    <td>${issue.state}</td>
+    </tr>`
+  )
+  .join('');
